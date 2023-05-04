0d1e53d604e5cb35cba09dce9508276c62dd08d6
Jenifer Melghem <125829408+JenMelghem@users.noreply.github.com>
2023-05-03 22:06:24 -0600

    Add files via upload

---
+++ Prognet/Conexion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prognet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/inventario";
+        String usuario = "root";
+        String contrasena = "";
+        
+        Connection conexion = DriverManager.getConnection(url, usuario, contrasena);
+        
+        return conexion;
+    }
+    
+}
